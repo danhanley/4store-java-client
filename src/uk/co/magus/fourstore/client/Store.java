5963e3571abc9c45db22a4afd9ea3aa9c50357a1
Dan Hanley <dan.hanley@magus.co.uk>
2009-10-23 13:07:07 +0100

    Added BSD licencing

--- src/uk/co/magus/fourstore/client/Store.java
+++ src/uk/co/magus/fourstore/client/Store.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright (c) 2009, Magus Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ *    Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ *  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package uk.co.magus.fourstore.client;
 
 import java.io.BufferedReader;
@@ -13,7 +33,7 @@ import java.net.URLEncoder;
 /**
  * Provides a basic client wrapper for the 4store httpd service
  * 
- * @author Dan
+ * @author Dan Hanley - dan.hanley @ magus.co.uk
  * 
  */
 public class Store {
